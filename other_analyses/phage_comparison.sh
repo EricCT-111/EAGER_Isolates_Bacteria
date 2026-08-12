11c255981eaf234ac7fe58b4f5b71c85b8426cc8
EricCT-111 <fzn25002@uconn.edu>
2026-08-12 09:03:31 -0400

    Update phage_comparison.sh

--- other_analyses/phage_comparison.sh
+++ other_analyses/phage_comparison.sh
@@ -1,19 +1,19 @@
 # comparing bacillus phage scm10 with B83 and BMBtp14
-source /labs/Hird/usr/miniforge3/etc/profile.d/conda.sh
-conda activate /labs/Hird/usr/EAGER_sequences/project_tools/fetch_env
+source /path/usr/miniforge3/etc/profile.d/conda.sh
+conda activate /path/EAGER_sequences/project_tools/fetch_env
 
 # EDIRECT - to pull viral genomes
 efetch -db nuccore -id NC_XXXXX -format fasta > B83.fna
 
 conda deactivate
-conda activate /labs/Hird/usr/EAGER_sequences/project_tools/phage_env
+conda activate /path/EAGER_sequences/project_tools/phage_env
 # run pharokka on B83 and BMBtp14
 conda deactivate
-conda activate /labs/Hird/usr/EAGER_sequences/project_tools/phold_env
+conda activate path/EAGER_sequences/project_tools/phold_env
 # run phold on B83 and BMBtp14
 
 conda deactivate
-conda activate /labs/Hird/usr/EAGER_sequences/project_tools/analysis_env
+conda activate /path/EAGER_sequences/project_tools/analysis_env
 
 # DNA Comparisons with MUMmer
 dnadiff -p comparison query.fna ref.fna
@@ -37,5 +37,5 @@ phmmer --tblout B83_vs_BMB.tbl  --domtblout B83_vs_BMB.domtbl  B83.faa BMBtp14.f
 conda deactivate
 # ORTHOFINDER comparison
 # move all 3 protein files into same directory
-conda activate /labs/Hird/usr/EAGER_sequences/project_tools/orthofinder_env
+conda activate /path/EAGER_sequences/project_tools/orthofinder_env
 orthofinder -f <directory_name>fetch_env
