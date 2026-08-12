58744575441191e641e318d2ee9c2da195e4a710
EricCT-111 <fzn25002@uconn.edu>
2026-08-12 06:23:21 -0400

    Update 19_mge.sh

--- scripts/19_mge.sh
+++ scripts/19_mge.sh
@@ -15,7 +15,7 @@
 # Run one sample: sbatch mge.sh <sample_id>
 # Run whole batch: sbatch --array=1-15 mge.sh
 # -----------------------------------------------------------------------------
-PIPELINE_DIR="${PIPELINE_DIR:-/labs/Hird/usr/EAGER_sequences/scripts}"
+PIPELINE_DIR="${PIPELINE_DIR:-/path/EAGER_sequences/scripts}"
 source "${PIPELINE_DIR}/lib.sh"
 pipeline_init "$@"
 activate_env "$MGE_ENV"
