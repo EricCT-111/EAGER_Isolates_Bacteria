960f6affbdc615662b508d11470f7139c8ee61aa
EricCT-111 <fzn25002@uconn.edu>
2026-08-12 09:02:39 -0400

    Update ani_comparison.sh

--- other_analyses/ani_comparison.sh
+++ other_analyses/ani_comparison.sh
@@ -1,6 +1,6 @@
 # Based on accession numbers from btyper3
-source /labs/Hird/usr/miniforge3/etc/profile.d/conda.sh
-conda activate /labs/Hird/usr/EAGER_sequences/project_tools/fetch_env
+source /path/miniforge3/etc/profile.d/conda.sh
+conda activate /path/EAGER_sequences/project_tools/fetch_env
 
 datasets download genome accession GCF_XXXXXXX --include genome #gff3,rna,cds,protein,genome,seq-report
 unzip ncbi_dataset.zip
@@ -8,7 +8,7 @@ unzip ncbi_dataset.zip
 conda deactivate
 # Skani triangle for ani matrix
 # combine all genomes of interest into same directory
-conda activate /labs/Hird/usr/EAGER_sequences/project_tools/taxonomy_env
+conda activate /path/EAGER_sequences/project_tools/taxonomy_env
 skani triangle all_genomes/*.fna -o ani_matrix.txt -t 16 --full-matrix
 
 # skani github python script for generating hierarchical clustered ani matrix heatmap
