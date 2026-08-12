4ab80d82f1facdd6c64f9796424cebd7dde3597c
EricCT-111 <fzn25002@uconn.edu>
2026-08-12 05:47:56 -0400

    Update conda_setup.sh paths for new installation location

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,21 +1,20 @@
 # ---------------------------------------------------------------------------
 # One-time Conda Setup
-#
 # Why: 
-#  The shared module (miniconda/311_23.11.0-2) is conda 23.11.0 (out of date)
+#  The shared module (miniconda/311_23.11.0-2) is out of date
 #    - Can cause issues for environment downloads
 #    - Doesnt have integrated libmamba
 #    - Do not module load miniconda alongside the new download
 # ---------------------------------------------------------------------------
 
 # 1. Install Miniforge (current conda + the libmamba solver)
-cd /labs/Hird/usr
+cd /path
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
-bash Miniforge3-Linux-x86_64.sh -b -p /labs/Hird/usr/miniforge3
+bash Miniforge3-Linux-x86_64.sh -b -p /path/miniforge3
 rm Miniforge3-Linux-x86_64.sh
 
 # 2. Put it ahead of the hpc conda
-echo 'export PATH="/labs/Hird/usr/miniforge3/condabin:$PATH"' >> ~/.bashrc
+echo 'export PATH="/path/miniforge3/condabin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 # 3. Make sure this path is set in env_install.sh and paths.config
